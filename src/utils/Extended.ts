544c7e4229b27644be37a8162dbc4558cadd21c2
Pepijn98 <root@kurozeropb.info>
2020-08-12 13:40:16 +0200

    Remove old eslint commentw

--- src/utils/Extended.ts
+++ src/utils/Extended.ts
@@ -1,21 +1,18 @@
-import { Guild } from "eris";
 import settings from "~/settings";
+import { Guild } from "eris";
 
 /** Capitalize the first letter of a string */
 String.prototype.capitalize = function (): string {
-    // eslint-disable-line no-extend-native
     return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
 /** Paginate over an array */
 Array.prototype.paginate = function <T>(pageSize: number, pageNumber: number): T[] {
-    // eslint-disable-line no-extend-native
     --pageNumber;
     return this.slice(pageNumber * pageSize, (pageNumber + 1) * pageSize);
 };
 
 Array.prototype.remove = function <T>(item: T): T[] {
-    // eslint-disable-line no-extend-native
     for (let i = 0; i < this.length; i++) {
         if (this[i] === item) this.splice(i, 1);
     }
