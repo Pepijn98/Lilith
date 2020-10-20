9171549b7da5f8ab907192963e3c1dc7949b461d
Pepijn98 <root@kurozeropb.info>
2020-10-20 15:03:02 +0200

    Handle uncaughtExceptions

--- src/index.ts
+++ src/index.ts
@@ -19,64 +19,84 @@ const eventLoader = new EventLoader(client);
 const commandLoader = new CommandLoader(client);
 const commandHandler = new CommandHandler(client);
 
+client.on("ready", async () => {
+    if (!ready) {
+        // Load commands
+        client.guildPrefixMap = await loadPrefixes();
+        client.commands = await commandLoader.load(path.join(__dirname, "commands"));
+
+        // Log some info
+        logger.ready(`Logged in as ${client.user.tag}`);
+        logger.ready(`Loaded [${client.commands.size}] commands`);
+
+        client.editStatus("online", { name: "Diablo III", type: 0 });
+
+        await postGuildCount(client);
+
+        // We're ready \o/
+        ready = true;
+    }
+});
+
+// Handle commands
+client.on("messageCreate", async (msg) => {
+    if (!ready) return; // Bot not ready yet
+    if (!msg.author) return; // Probably system message
+    if (msg.author.discriminator === "0000") return; // Probably a webhook
+    if (msg.author.id === client.user.id) return; // Ignore our own messages
+
+    client.stats.messagesSeen++;
+
+    // Check if message was send in a guild channel
+    let prefix = settings.prefix;
+    if (isGuildChannel(msg.channel)) {
+        prefix = msg.channel.guild.prefix;
+        // If message starts with configured prefix handleCommand
+        if (msg.content.startsWith(prefix)) {
+            await commandHandler.handleCommand(msg, prefix, false);
+        }
+    } else if (isDMChannel(msg.channel) && msg.content.startsWith(settings.prefix)) {
+        await commandHandler.handleCommand(msg, prefix, true);
+    }
+});
+
+// Handle disconnects
+client.on("disconnect", () => {
+    logger.warn("DISCONNECT", "Client disconnected");
+});
+
+/**
+ * Sometimes when a shard goes down for a moment and comes back up is loses it's status
+ * so we re-add it here
+ */
+client.on("shardResume", (id: number) => {
+    const shard = client.shards.get(id);
+    if (shard) {
+        shard.editStatus("online", { name: "Diablo III", type: 0 });
+    }
+});
+
+process.on("unhandledRejection", (reason) => {
+    logger.error("UNHANDLED_REJECTION", reason as any);
+});
+
+process.on("uncaughtException", (e) => {
+    logger.error("UNCAUGHT_EXCEPTION", e);
+});
+
+process.on("SIGINT", () => {
+    client.disconnect({ reconnect: false });
+    process.exit(0);
+});
+
 async function main(): Promise<void> {
     await client.setup();
 
     // Load all events besides ready and messageCreate
     await eventLoader.load(path.join(__dirname, "events"));
 
-    client.on("ready", async () => {
-        if (!ready) {
-            // Load commands
-            client.guildPrefixMap = await loadPrefixes();
-            client.commands = await commandLoader.load(path.join(__dirname, "commands"));
-
-            // Log some info
-            logger.ready(`Logged in as ${client.user.tag}`);
-            logger.ready(`Loaded [${client.commands.size}] commands`);
-
-            await postGuildCount(client);
-
-            // We're ready \o/
-            ready = true;
-        }
-
-        client.editStatus("online", { name: "Diablo III", type: 0 });
-    });
-
-    // Handle commands
-    client.on("messageCreate", async (msg) => {
-        if (!ready) return; // Bot not ready yet
-        if (!msg.author) return; // Probably system message
-        if (msg.author.discriminator === "0000") return; // Probably a webhook
-        if (msg.author.id === client.user.id) return; // Ignore our own messages
-
-        client.stats.messagesSeen++;
-
-        // Check if message was send in a guild channel
-        let prefix = settings.prefix;
-        if (isGuildChannel(msg.channel)) {
-            prefix = msg.channel.guild.prefix;
-            // If message starts with configured prefix handleCommand
-            if (msg.content.startsWith(prefix)) {
-                await commandHandler.handleCommand(msg, prefix, false);
-            }
-        } else if (isDMChannel(msg.channel) && msg.content.startsWith(settings.prefix)) {
-            await commandHandler.handleCommand(msg, prefix, true);
-        }
-    });
-
-    process.on("unhandledRejection", (reason) => {
-        logger.error("UNHANDLED_REJECTION", reason as any);
-    });
-
-    process.on("SIGINT", () => {
-        client.disconnect({ reconnect: false });
-        process.exit(0);
-    });
-
     // Connect to discord OwO
-    client.connect().catch((e) => logger.error("CONNECT", e.stack));
+    client.connect().catch((e) => logger.error("CONNECT", e));
 }
 
 main().catch((e) => logger.error("MAIN", e));
